ea1e78a3987dc443acd55b804d35a4b13d9baf43
Victor1128 <victorbadulescu7@gmail.com>
2024-04-20 20:41:56 +0300

    added input and gets predictions from server

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,19 +2,59 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const LandingPage = () => {
-  const [message, setMessage] = useState("");
+  const [title, setTitle] = useState("");
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
 
-  const getRequest = async () => {
-    const response = await axios.get(`http://localhost:8000/hello/Vincent`);
-    setMessage(response.data["message"]);
-    console.log(response.data);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const requestBody = {
+      title: title,
+    };
+
+    try {
+      const response = await axios.post(
+        `http://localhost:8000/predict`,
+        requestBody
+      );
+      setResponse(response.data.predictions[0]);
+    } catch (err) {
+      console.log(err);
+      if (err.response) {
+        if (err.response.status === 403) {
+          setError("Forbidden.");
+        }
+        setError("Server error.");
+      } else {
+        setError("Unknown error.");
+      }
+    }
   };
 
   useEffect(() => {
-    getRequest();
-  }, []);
+    setError(null);
+  }, [error]);
 
-  return <div>{message}</div>;
+  return (
+    <main>
+      <h2>Introduceți titlul unui articol</h2>
+      <input
+        type="text"
+        id="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Caută</button>
+      {response && (
+        <div>
+          <h3>Rezultat</h3>
+          <p>Non-satira: {response[0]}</p>
+          <p>Satira: {response[1]}</p>
+        </div>
+      )}
+      {error && <p>{error}</p>}
+    </main>
+  );
 };
 
 export default LandingPage;
